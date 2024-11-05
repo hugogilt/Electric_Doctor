d14a7403662540c60b7992ee9e712f12334d5203
Hugo <hugogiltejero@gmail.com>
2024-11-05 17:25:07 +0100

    refactor(slider): refactorizo el slider de servicios(V0.3.1)

--- js/index.js
+++ js/index.js
@@ -1,106 +1,321 @@
 //Detalles servicios
 const originalContainers = document.querySelectorAll("#cards-containers .rows");
-const cardsContainer = document.getElementById("cards-containers");
+const contenidoServicios = document.getElementById("contenido-servicios");
 
 // Objeto que mapea los IDs de los botones a sus respectivas URLs
 const buttonUrls = {
-    "detalles-reparacion-motos": './servicios.html?servicios=reparacion-motos',
-    "detalles-reparacion-baterias": './servicios.html?servicios=reparacion-baterias',
-    "detalles-reparacion-patinetes": './servicios.html?servicios=reparacion-patinetes',
-    "detalles-envios": './servicios.html?servicios=envios',
-    "detalles-pre-itvs": './servicios.html?servicios=pre-itvs',
-    "detalles-neumaticos": './servicios.html?servicios=neumaticos',
-    "detalles-puestas-a-punto": './servicios.html?servicios=puestas-a-punto',
-    "detalles-revisiones": './servicios.html?servicios=revisiones',
+  "detalles-reparacion-motos": './servicios.html?servicios=reparacion-motos',
+  "detalles-reparacion-baterias": './servicios.html?servicios=reparacion-baterias',
+  "detalles-reparacion-patinetes": './servicios.html?servicios=reparacion-patinetes',
+  "detalles-envios": './servicios.html?servicios=envios',
+  "detalles-pre-itvs": './servicios.html?servicios=pre-itvs',
+  "detalles-neumaticos": './servicios.html?servicios=neumaticos',
+  "detalles-puestas-a-punto": './servicios.html?servicios=puestas-a-punto',
+  "detalles-revisiones": './servicios.html?servicios=revisiones',
 };
 // Función para asignar event listeners a los botones
 function assignEventListeners() {
-    Object.entries(buttonUrls).forEach(([buttonId, url]) => {
-        const button = document.getElementById(buttonId);
-        if (button) {
-            button.addEventListener('click', function () {
-                window.location.href = url;
-            });
-        }
-    });
+  Object.entries(buttonUrls).forEach(([buttonId, url]) => {
+    const button = document.getElementById(buttonId);
+    if (button) {
+      button.addEventListener('click', function () {
+        window.location.href = url;
+      });
+    }
+  });
 }
 
 // Llamar a la función para asignar event listeners al cargar la página
 assignEventListeners();
 
-function createSlidingCards() {
-    // Crear un div para la card deslizable
-    const slidingCard = document.createElement("div");
-    slidingCard.className = "sliding-card";
-    slidingCard.style.display = "flex"; // Hacerlo flexible para el desplazamiento
 
-    // Recorrer cada contenedor y agregar las cards a la card deslizable
-    originalContainers.forEach(container => {
-        const cards = container.querySelectorAll(".card");
-        cards.forEach(card => {
-            // Clonar la card para añadirla al slidingCard
-            const clonedCard = card.cloneNode(true);
-            slidingCard.appendChild(clonedCard);
 
-            // Asignar el event listener del botón de la tarjeta original al botón de la tarjeta clonada
-            const originalButton = card.querySelector("button");
-            const buttonId = originalButton.id;
-            const url = buttonUrls[buttonId];
-            if (url) {
-                clonedCard.querySelector("button").addEventListener("click", function () {
-                    window.location.href = url;
-                });
-            }
-        });
-    });
+//SLIDER
+function crearSliderServicios(containerId) {
+  contenidoServicios.style.position = 'relative'
+  // Crear el contenedor principal del swiper
+  const swiperContainer = document.createElement('div');
+  swiperContainer.className = 'swiper-container';
+  swiperContainer.id = 'swiper-container';
 
-    // Agregar la card deslizable al contenedor principal
-    cardsContainer.appendChild(slidingCard);
 
-    // Aplicar estilos de deslizamiento
-    slidingCard.style.overflowX = "auto"; // Permitir el desplazamiento horizontal
-    slidingCard.style.scrollSnapType = "x mandatory"; // Para el efecto de 'snap'
+  // Crear el contenedor de las slides
+  const swiperWrapper = document.createElement('div');
+  swiperWrapper.className = 'swiper-wrapper';
 
-    // Estilo de cada card
-    slidingCard.querySelectorAll(".card").forEach(card => {
-        card.style.scrollSnapAlign = "start"; // Alineación al deslizar
-        card.style.minWidth = "300px"; // Ajustar el ancho mínimo de cada card
-        card.style.margin = "0 10px"; // Margen entre cards
-    });
+  // Definir los servicios y sus detalles
+  const servicios = [
+      {
+          id: 'reparacion-motos',
+          imgSrc: 'images/moto.png',
+          alt: 'Moto',
+          title: 'Reparación de Motos',
+          buttonId: 'detalles-reparacion-motos'
+      },
+      {
+          id: 'reparacion-baterias',
+          imgSrc: 'images/bateria.png',
+          alt: 'Bateria',
+          title: 'Reparación de Baterías',
+          buttonId: 'detalles-reparacion-baterias'
+      },
+      {
+          id: 'reparacion-patinetes',
+          imgSrc: 'images/patinete.png',
+          alt: 'Patinete',
+          title: 'Reparación de Patinetes',
+          buttonId: 'detalles-reparacion-patinetes'
+      },
+      {
+          id: 'envios',
+          imgSrc: 'images/camion.png',
+          alt: 'Camion',
+          title: 'Envío de Motos a toda la península',
+          buttonId: 'detalles-envios'
+      },
+      {
+          id: 'pre-itvs',
+          imgSrc: 'images/pre-itv.png',
+          alt: 'pre-itv',
+          title: 'Pre-ITVS',
+          buttonId: 'detalles-pre-itvs'
+      },
+      {
+          id: 'neumaticos',
+          imgSrc: 'images/neumatico.png',
+          alt: 'neumatico',
+          title: 'Neumáticos',
+          buttonId: 'detalles-neumaticos'
+      },
+      {
+          id: 'puestas-a-punto',
+          imgSrc: 'images/puesta-a-punto.png',
+          alt: 'Puesta a Punto',
+          title: 'Puestas a Punto',
+          buttonId: 'detalles-puestas-a-punto'
+      },
+      {
+          id: 'revisiones',
+          imgSrc: 'images/revision.png',
+          alt: 'Revision',
+          title: 'Revisiones',
+          buttonId: 'detalles-revisiones'
+      }
+  ];
 
-    // Ocultar las filas originales
-    originalContainers.forEach(container => {
-        container.style.display = "none";
-    });
+  // Crear las cards y añadirlas al swiperWrapper
+  servicios.forEach(servicio => {
+      const card = document.createElement('div');
+      card.className = 'card swiper-slide'; // Agregar la clase swiper-slide a cada card
+      card.id = servicio.id;
+
+      const img = document.createElement('img');
+      img.src = servicio.imgSrc;
+      img.alt = servicio.alt;
+
+      const title = document.createElement('h3');
+      title.textContent = servicio.title;
+
+      const button = document.createElement('button');
+      button.id = servicio.buttonId;
+      button.textContent = 'Detalles';
+
+      // Estructurar la tarjeta
+      card.appendChild(img);
+      card.appendChild(title);
+      card.appendChild(button);
+      swiperWrapper.appendChild(card); // Añadir la card directamente al swiperWrapper
+  });
+
+  // Añadir la wrapper al contenedor principal
+  swiperContainer.appendChild(swiperWrapper);
+
+  // Crear y añadir la paginación
+  const pagination = document.createElement('div');
+  pagination.className = 'swiper-pagination';
+  swiperContainer.appendChild(pagination);
+
+
+  // Insertar el slider en el contenedor deseado
+  containerId.appendChild(swiperContainer);
+
+  // Inicializar Swiper
+  const swiper = new Swiper('.swiper-container', {
+    loop: true, // Activa el modo de bucle infinito
+    slidesPerView: 1.2, // Solo una carta visible a la vez
+    centeredSlides: true, // Centra el slide activo
+    spaceBetween: 10, // Espacio entre cartas en px
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Permite hacer clic en los puntos de paginación
+    },
+    grabCursor: true, // Cambia el cursor a una mano cuando se pasa sobre el carrusel
+    touchEventsTarget: 'container', // Habilita el gesto de arrastre en el contenedor
+    touchRatio: 1, // Ajusta la sensibilidad del deslizamiento
+    // Habilitar el desplazamiento táctil
+    simulateTouch: true, // Asegura que el deslizamiento táctil funcione
+    noSwiping: false, // Permite el deslizamiento en todo el contenedor
+  });
 }
 
-function resetCards() {
-    // Eliminar la card deslizable
-    const slidingCard = document.querySelector(".sliding-card");
-    if (slidingCard) {
-        slidingCard.remove();
-    }
 
-    // Restaurar las filas originales
-    originalContainers.forEach(container => {
-        container.style.removeProperty('display'); // Mostrar las filas originales
-        cardsContainer.appendChild(container); // Reagregar al contenedor
-    });
-    
-    // Re-asignar event listeners a los botones originales
-    assignEventListeners();
+function crearContenidoServicios(parent) {
+  // Crear el contenedor de las tarjetas
+  const cardsContainers = document.createElement('div');
+  cardsContainers.id = 'cards-containers';
+
+  // Crear el primer contenedor de servicios
+  const containerServicios1 = document.createElement('div');
+  containerServicios1.id = 'container-servicios-1';
+  containerServicios1.className = 'rows';
+
+  // Servicios del primer contenedor
+  const servicios1 = [
+      {
+          id: 'reparacion-motos',
+          imgSrc: 'images/moto.png',
+          alt: 'Moto',
+          title: 'Reparación de Motos',
+          buttonId: 'detalles-reparacion-motos'
+      },
+      {
+          id: 'reparacion-baterias',
+          imgSrc: 'images/bateria.png',
+          alt: 'Bateria',
+          title: 'Reparación de Baterías',
+          buttonId: 'detalles-reparacion-baterias'
+      },
+      {
+          id: 'reparacion-patinetes',
+          imgSrc: 'images/patinete.png',
+          alt: 'Patinete',
+          title: 'Reparación de Patinetes',
+          buttonId: 'detalles-reparacion-patinetes'
+      },
+      {
+          id: 'envios',
+          imgSrc: 'images/camion.png',
+          alt: 'Camion',
+          title: 'Envío de Motos a toda la península',
+          buttonId: 'detalles-envios'
+      }
+  ];
+
+  // Crear y añadir las tarjetas al primer contenedor
+  servicios1.forEach(servicio => {
+      const card = document.createElement('div');
+      card.id = servicio.id;
+      card.className = 'card';
+
+      const img = document.createElement('img');
+      img.src = servicio.imgSrc;
+      img.alt = servicio.alt;
+
+      const title = document.createElement('h3');
+      title.textContent = servicio.title;
+
+      const button = document.createElement('button');
+      button.id = servicio.buttonId;
+      button.textContent = 'Detalles';
+
+      // Estructurar la tarjeta
+      card.appendChild(img);
+      card.appendChild(title);
+      card.appendChild(button);
+      containerServicios1.appendChild(card);
+  });
+
+  // Añadir el primer contenedor de servicios al contenedor de tarjetas
+  cardsContainers.appendChild(containerServicios1);
+
+  // Crear el segundo contenedor de servicios
+  const containerServicios2 = document.createElement('div');
+  containerServicios2.id = 'container-servicios-2';
+  containerServicios2.className = 'rows';
+
+  // Servicios del segundo contenedor
+  const servicios2 = [
+      {
+          id: 'pre-itvs',
+          imgSrc: 'images/pre-itv.png',
+          alt: 'pre-itv',
+          title: 'Pre-ITVS',
+          buttonId: 'detalles-pre-itvs'
+      },
+      {
+          id: 'neumaticos',
+          imgSrc: 'images/neumatico.png',
+          alt: 'neumatico',
+          title: 'Neumáticos',
+          buttonId: 'detalles-neumaticos'
+      },
+      {
+          id: 'puestas-a-punto',
+          imgSrc: 'images/puesta-a-punto.png',
+          alt: 'Puesta a Punto',
+          title: 'Puestas a Punto',
+          buttonId: 'detalles-puestas-a-punto'
+      },
+      {
+          id: 'revisiones',
+          imgSrc: 'images/revision.png',
+          alt: 'Revision',
+          title: 'Revisiones',
+          buttonId: 'detalles-revisiones'
+      }
+  ];
+
+  // Crear y añadir las tarjetas al segundo contenedor
+  servicios2.forEach(servicio => {
+      const card = document.createElement('div');
+      card.id = servicio.id;
+      card.className = 'card';
+
+      const img = document.createElement('img');
+      img.src = servicio.imgSrc;
+      img.alt = servicio.alt;
+
+      const title = document.createElement('h3');
+      title.textContent = servicio.title;
+
+      const button = document.createElement('button');
+      button.id = servicio.buttonId;
+      button.textContent = 'Detalles';
+
+      // Estructurar la tarjeta
+      card.appendChild(img);
+      card.appendChild(title);
+      card.appendChild(button);
+      containerServicios2.appendChild(card);
+  });
+
+  // Añadir el segundo contenedor de servicios al contenedor de tarjetas
+  cardsContainers.appendChild(containerServicios2);
+
+  // Insertar el contenido en el padre especificado
+      parent.appendChild(cardsContainers);
+  
 }
 
 function handleResize() {
-    if (window.innerWidth <= 750) {
-        // Crear cards deslizable si el ancho es menor o igual a 750px
-        if (!document.querySelector(".sliding-card")) {
-            createSlidingCards();
-        }
-    } else {
-        // Restaurar la estructura original si el ancho es mayor a 750px
-        resetCards();
+  let htmlWidth = parseFloat(window.getComputedStyle(document.documentElement).width);
+  if (htmlWidth <= 750) {
+    // Crear cards deslizable si el ancho es menor o igual a 750px
+    if (!document.querySelector("#swiper-container")) {
+      if (document.querySelector("#cards-containers")) {
+        document.querySelector("#cards-containers").remove();
+      }
+      crearSliderServicios(contenidoServicios);
     }
+  } else {
+    // Restaurar la estructura original si el ancho es mayor a 750px
+    if (!document.querySelector('#cards-containers')) {
+      if (document.querySelector('#swiper-container')) {
+        document.querySelector('#swiper-container').remove();
+      }
+      crearContenidoServicios(contenidoServicios);
+    }
+  }
 }
 
 // Manejar el evento de redimensionamiento
