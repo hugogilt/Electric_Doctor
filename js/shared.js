9b8c8441b6943dd52a6ab4b6c99a5e2bd1ec84b1
Hugo <hugogiltejero@gmail.com>
2025-02-22 13:08:40 +0100

    fix(contraseña): permito caracteres especiales

--- js/shared.js
+++ js/shared.js
@@ -802,7 +802,7 @@ registerForm.addEventListener("submit", async function (event) {
   }
 
   // Validar que la contraseña tenga al menos 8 caracteres, una mayúscula, una minúscula y un número
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
   if (!passwordRegex.test(password)) {
     passwordErrorMessageRegister.textContent = 'La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.';
     formatoInadecuado = true;
